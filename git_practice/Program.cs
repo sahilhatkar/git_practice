8485d3814ec9f2808b2ccf42fcde8f991d56b806
hp <you@example.com>
2021-11-01 10:14:48 +0530

    Add project files.

---
+++ git_practice/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace git_practice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("hello");
+        }
+    }
+}
